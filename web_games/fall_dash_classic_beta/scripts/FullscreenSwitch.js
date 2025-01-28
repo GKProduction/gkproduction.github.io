8753f058099e7753f523bb56e485578cec3a398e
GKProduction <glebkapustin64@gmail.com>
2025-01-28 12:52:19 +0300

    Fall Dash - Classic v1.3 Beta 2.0

--- web_games/fall_dash_classic_beta/scripts/FullscreenSwitch.js
+++ web_games/fall_dash_classic_beta/scripts/FullscreenSwitch.js
@@ -1,5 +1,6 @@
 // Fullscreen Switch
 
+// Переключение полноэкранного режима
 function setFullscreen(fullscreen_mode) {
     if (fullscreen_mode) {
         var el = document.documentElement,
@@ -7,8 +8,24 @@ function setFullscreen(fullscreen_mode) {
              || el.webkitRequestFullScreen
              || el.mozRequestFullScreen
              || el.msRequestFullscreen;
-        rfs.call(el).catch((err) => err=null);;
+        rfs.call(el).catch((err) => err = null); ;
     } else {
-        document.exitFullscreen().catch((err) => err=null);
+        document.exitFullscreen().catch((err) => err = null);
+    }
+}
+
+// Выход из полноэкранного режима
+if (document.addEventListener) {
+    document.addEventListener('fullscreenchange', exitHandler, false);
+    document.addEventListener('mozfullscreenchange', exitHandler, false);
+    document.addEventListener('MSFullscreenChange', exitHandler, false);
+    document.addEventListener('webkitfullscreenchange', exitHandler, false);
+}
+
+function exitHandler() {
+    if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+        info_event = "On Exit Fullscreen";
+        console.log("On Exit Fullscreen");
+        console.log("!!!: "+info_event);
     }
 }
